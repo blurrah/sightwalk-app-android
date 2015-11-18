071bb6d0be83078986dccacc17f89c5542b68a91
Jeroen Doorn <snelle18@gmail.com>
2015-11-18 14:10:34 +0100

    Added validation for register screen

--- app/src/main/java/net/sightwalk/Controllers/Introduction/RegisterActivity.java
+++ app/src/main/java/net/sightwalk/Controllers/Introduction/RegisterActivity.java
@@ -5,16 +5,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.*;
 
 import net.sightwalk.Controllers.Dashboard.DashboardActivity;
 import net.sightwalk.R;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    Validator validator;
+
+    @NotEmpty(message = "Verplicht veld")
+    @Email(message = "Ongeldige email")
+    @Bind(R.id.emailEditText) EditText email;
+
+    @NotEmpty(message = "Verplicht veld")
+    @Length(min = 3, max = 10, message = "Gebruikersnaam tussen 3 en 10 karakters")
+    @Bind(R.id.usernameEditText) EditText username;
+
+    @NotEmpty(message = "Verplicht veld")
+    @Length(min = 5, max = 20, message = "Wachtwoord tussen 5 en 20 karakters")
+    @Password(scheme = Password.Scheme.ANY, message = "Ongeldige wachtwoord")
+    @Bind(R.id.passwordEditText) EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
 
         Button rButton = (Button) findViewById(R.id.registerButton);
         rButton.setOnClickListener(new registerListener());
@@ -23,8 +54,28 @@ public class RegisterActivity extends AppCompatActivity {
     private class registerListener implements Button.OnClickListener {
         @Override
         public void onClick(View v) {
-            Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
-            startActivity(i);
+
+            validator.validate();
+        }
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
