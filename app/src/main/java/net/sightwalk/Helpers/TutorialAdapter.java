0939ffcc1faa837e7cc1b1a2173ea789fbb82c2f
rubendehaas <rdehaas92@gmail.com>
2015-11-20 11:42:56 +0100

    Tutorial slider, indicator

--- app/src/main/java/net/sightwalk/Helpers/TutorialAdapter.java
+++ app/src/main/java/net/sightwalk/Helpers/TutorialAdapter.java
@@ -1,18 +1,23 @@
 package net.sightwalk.Helpers;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.widget.TextView;
 
 import net.sightwalk.Controllers.Tutorial.TutorialFragment;
+import net.sightwalk.R;
 
 public class TutorialAdapter extends FragmentStatePagerAdapter {
 
     int mNumOfTabs;
+    Context ctx;
 
     public TutorialAdapter(FragmentManager fm, int NumOfTabs) {
         super(fm);
         this.mNumOfTabs = NumOfTabs;
+
     }
 
     @Override
@@ -21,13 +26,17 @@ public class TutorialAdapter extends FragmentStatePagerAdapter {
         switch (position) {
             case 0:
                 TutorialFragment frag1 = new TutorialFragment();
+                frag1.setTutorialImg(R.drawable.tutorial_1);
                 return frag1;
             case 1:
                 TutorialFragment frag2 = new TutorialFragment();
+                frag2.setTutorialImg(R.drawable.tutorial_2);
                 return frag2;
             default:
                 return null;
         }
+
+
     }
 
     @Override
