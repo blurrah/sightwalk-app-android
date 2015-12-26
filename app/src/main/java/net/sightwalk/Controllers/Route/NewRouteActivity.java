834017e21b89fe31b13dfbcbbfbdf8d932fa8f92
Frank Kuipers <frank.k994@gmail.com>
2015-12-26 16:26:05 +0100

    Implemented gps tracker

--- app/src/main/java/net/sightwalk/Controllers/Route/NewRouteActivity.java
+++ app/src/main/java/net/sightwalk/Controllers/Route/NewRouteActivity.java
@@ -23,31 +23,26 @@ import com.google.android.gms.maps.model.*;
 
 import net.sightwalk.Controllers.Dashboard.DashboardActivity;
 import net.sightwalk.Controllers.SettingsActivity;
+import net.sightwalk.Helpers.GPSTracker;
+import net.sightwalk.Helpers.GPSTrackerInterface;
+import net.sightwalk.Helpers.PermissionActivity;
 import net.sightwalk.Models.*;
 import net.sightwalk.R;
 import net.sightwalk.Tasks.RouteTask;
 
-public class NewRouteActivity extends AppCompatActivity {
+public class NewRouteActivity extends PermissionActivity implements GPSTrackerInterface {
 
     private String waypoint;
     private StringBuilder builder;
     private CheckBox checkBox;
     private LatLng location;
-    private locationListener listener;
-    private LocationManager mLocationManager;
-    private UserLocation mLocation;
 
-    private static AlertDialog alert;
+    private GPSTracker gpsTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_route);
-
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        listener = new locationListener();
-
-        locationManager();
 
         checkBox = (CheckBox) findViewById(R.id.cbRouteDestination);
         checkBox.setOnCheckedChangeListener(new checkedListener());
@@ -57,17 +52,23 @@ public class NewRouteActivity extends AppCompatActivity {
 
         Button cButton = (Button) findViewById(R.id.chooseRouteButton);
         cButton.setOnClickListener(new chooseListener());
+
+        gpsTracker = new GPSTracker(this, this);
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
 
+        updateDistance();
+    }
+
+    private void updateDistance() {
         Button routeButton = (Button) findViewById(R.id.routeButton);
         TextView amountSights = (TextView) findViewById(R.id.tvAmountSights);
         amountSights.setText("Totaal " + Sights.mSightList.size() + " sights");
 
-        if(Sights.getInstance().mSightList.size() == 0) {
+        if (Sights.getInstance().mSightList.size() == 0) {
             routeButton.setEnabled(false);
             routeButton.setBackgroundColor(getResources().getColor(R.color.colorDisabled));
         } else {
@@ -78,7 +79,7 @@ public class NewRouteActivity extends AppCompatActivity {
         }
     }
 
-    public void createRoute(boolean cbChecked){
+    public void createRoute(boolean cbChecked) {
         builder = new StringBuilder();
 
         for (int i = 0; i < Sights.getInstance().mSightList.size(); i++) {
@@ -122,6 +123,12 @@ public class NewRouteActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void updatedLocation(Location location) {
+        this.location = new LatLng(location.getLatitude(), location.getLongitude());
+        updateDistance();
+    }
+
     private class routeListener implements Button.OnClickListener {
         @Override
         public void onClick(View v) {
@@ -140,85 +147,14 @@ public class NewRouteActivity extends AppCompatActivity {
         }
     }
 
-    private class checkedListener implements CheckBox.OnCheckedChangeListener{
+    private class checkedListener implements CheckBox.OnCheckedChangeListener {
 
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-            if(Sights.getInstance().mSightList.size() > 0){
+            if (Sights.getInstance().mSightList.size() > 0) {
                 createRoute(b);
             }
         }
     }
 
-    public void locationManager() {
-        boolean gpsIsEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        boolean networkIsEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-        if(gpsIsEnabled) {
-            try {
-                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, listener);
-            } catch (SecurityException e) {
-                Log.e("ERROR_", e.getLocalizedMessage());
-            }
-        }
-        else {
-            errorDialog("GPS locatie niet gevonden");
-        }
-        if (networkIsEnabled) {
-            try {
-                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, listener);
-            } catch (SecurityException e) {
-                Log.e("ERROR_", e.getLocalizedMessage());
-            }
-        }
-        else {
-            errorDialog("Netwerklocatie niet gevonden");
-        }
-    }
-
-    private class locationListener implements LocationListener {
-        @Override
-        public void onLocationChanged(Location location) {
-            if(location == null) {
-                errorDialog("Locatie niet gevonden");
-            }
-            else {
-                mLocation = UserLocation.getInstance();
-                mLocation.userlocation = new LatLng(location.getLatitude(), location.getLongitude());
-            }
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) { }
-
-        @Override
-        public void onProviderEnabled(String provider) { }
-
-        @Override
-        public void onProviderDisabled(String provider) { }
-    }
-
-    public void errorDialog(String error) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setMessage("Om deze functie te gebruiken hebben we je locatie nodig. Zet alsjeblieft je internet of GPS modus aan in de locatie opties.")
-                .setTitle(error)
-                .setCancelable(false)
-                .setPositiveButton("Instellingen", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(intent);
-                        alert.dismiss();
-                    }
-                })
-                .setNegativeButton("Annuleren", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                    }
-                });
-        alert = builder.create();
-        alert.show();
-    }
 }
