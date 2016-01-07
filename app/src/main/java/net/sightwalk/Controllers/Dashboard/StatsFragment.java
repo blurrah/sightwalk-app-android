2b810de5afa6833efd6840ac63393e9620103e50
Jeroen Doorn <snelle18@gmail.com>
2016-01-07 13:28:56 +0100

    Basic chart for dashboard stats

--- app/src/main/java/net/sightwalk/Controllers/Dashboard/StatsFragment.java
+++ app/src/main/java/net/sightwalk/Controllers/Dashboard/StatsFragment.java
@@ -1,17 +1,93 @@
 package net.sightwalk.Controllers.Dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
 import net.sightwalk.R;
+
+import java.util.ArrayList;
 
 public class StatsFragment extends Fragment {
 
+    private LineChart chart;
+    private ArrayList<Entry> entries;
+    private ArrayList<String> labels;
+
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+        view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        chart = (LineChart) view.findViewById(R.id.chart);
+
+        setupChart();
+
+        return view;
+    }
+
+    private void setupChart() {
+        chart.setDescription("");
+        chart.setNoDataText("Er zijn helaas nog geen gegevens beschikbaar!");
+        chart.setNoDataTextDescription("...");
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getAxisRight().setEnabled(false);
+
+        XAxis label = chart.getXAxis();
+        label.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+
+        LineDataSet dataSet = new LineDataSet(getDataSet(), "Test");
+
+        dataSet.setDrawCubic(true);
+        dataSet.setLineWidth(1.75f);
+        dataSet.setCircleSize(3f);
+        dataSet.setColor(Color.BLUE);
+        dataSet.setCircleColor(Color.BLUE);
+        dataSet.setHighLightColor(Color.BLUE);
+        dataSet.setDrawValues(false);
+
+        LineData data = new LineData(getLabels(), dataSet);
+        chart.setData(data);
+    }
+
+    private ArrayList<Entry> getDataSet() {
+        entries = new ArrayList<>();
+
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(14f, 3));
+        entries.add(new Entry(10f, 4));
+        entries.add(new Entry(18f, 5));
+
+        return entries;
+    }
+
+    private ArrayList<String> getLabels() {
+        labels = new ArrayList<>();
+
+        labels.add("1");
+        labels.add("2");
+        labels.add("3");
+        labels.add("4");
+        labels.add("5");
+        labels.add("6");
+
+        return labels;
     }
 }
