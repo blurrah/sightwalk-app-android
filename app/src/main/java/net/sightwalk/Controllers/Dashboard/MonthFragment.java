f53d16fb10cab0d081124aa7246a93bb01256e27
Jeroen Doorn <snelle18@gmail.com>
2016-01-14 16:40:59 +0100

    Stats are working :)

--- app/src/main/java/net/sightwalk/Controllers/Dashboard/MonthFragment.java
+++ app/src/main/java/net/sightwalk/Controllers/Dashboard/MonthFragment.java
@@ -3,31 +3,56 @@ package net.sightwalk.Controllers.Dashboard;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
 import net.danlew.android.joda.JodaTimeAndroid;
+import net.sightwalk.Models.Activities;
 import net.sightwalk.R;
+import net.sightwalk.Stores.ActivitiesStore;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class MonthFragment extends Fragment {
 
     private BarChart chart;
+
+    private ActivitiesStore store;
+
+    private int valuesOne;
+    private int valuesTwo;
+    private int valuesThree;
+    private int valuesFour;
+    private int valuesFive;
+    private int valuesSix;
+
     private ArrayList<BarEntry> entries;
     private ArrayList<String> labels;
 
-    View view;
+    private ArrayList<Integer> valueOne = new ArrayList<>();
+    private ArrayList<Integer> valueTwo = new ArrayList<>();
+    private ArrayList<Integer> valueThree = new ArrayList<>();
+    private ArrayList<Integer> valueFour = new ArrayList<>();
+    private ArrayList<Integer> valueFive = new ArrayList<>();
+    private ArrayList<Integer> valueSix = new ArrayList<>();
+
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,11 +60,99 @@ public class MonthFragment extends Fragment {
 
         JodaTimeAndroid.init(getActivity());
 
+        store = new ActivitiesStore(getContext());
+
+        for(Activities activities : store.getAll()) {
+            checkDate(activities);
+        }
+
+        setValues();
+
         chart = (BarChart) view.findViewById(R.id.monthChart);
 
         setupChart();
 
         return view;
+    }
+
+    private void checkDate(Activities activities) {
+        DateTimeFormatter format = DateTimeFormat.forPattern("dd/MM/yyyy");
+        LocalDate date = format.parseLocalDate(activities.getActivityDate());
+        int month = date.getMonthOfYear();
+
+        int dateOne = DateTime.now().getMonthOfYear();
+        int dateTwo = DateTime.now().minusMonths(1).getMonthOfYear();
+        int dateThree = DateTime.now().minusMonths(2).getMonthOfYear();
+        int dateFour = DateTime.now().minusMonths(3).getMonthOfYear();
+        int dateFive = DateTime.now().minusMonths(4).getMonthOfYear();
+        int dateSix = DateTime.now().minusMonths(5).getMonthOfYear();
+
+        if(month == dateOne) {
+            valueOne.add(getDistance(activities.getJson()));
+        }
+        else if(month == dateTwo) {
+            valueTwo.add(getDistance(activities.getJson()));
+        }
+        else if(month == dateThree) {
+            valueThree.add(getDistance(activities.getJson()));
+        }
+        else if(month == dateFour) {
+            valueFour.add(getDistance(activities.getJson()));
+        }
+        else if(month == dateFive) {
+            valueFive.add(getDistance(activities.getJson()));
+        }
+        else if(month == dateSix) {
+            valueSix.add(getDistance(activities.getJson()));
+        }
+    }
+
+    private int getDistance(String json) {
+        int routedis = 0;
+
+        try {
+            JSONObject directionsObject = new JSONObject(json);
+
+            JSONArray routesObject = directionsObject.getJSONArray("routes");
+
+            for (int i = 0; i < routesObject.length(); i++) {
+                JSONObject overview = routesObject.getJSONObject(i);
+
+                JSONArray legsObject = overview.getJSONArray("legs");
+
+                for (int l = 0; l < legsObject.length(); l++) {
+                    JSONObject stepsobject = legsObject.getJSONObject(l);
+
+                    JSONObject routeDistance = stepsobject.getJSONObject("distance");
+
+                    routedis += routeDistance.getInt("value");
+                }
+            }
+        } catch (JSONException ex) {
+            Log.e("ERROR_", ex.getLocalizedMessage());
+        }
+        return routedis/1000;
+    }
+
+    private void setValues() {
+        for(Integer value : valueOne) {
+            valuesOne += value;
+        }
+        for(Integer value : valueTwo) {
+            valuesTwo += value;
+        }
+        for(Integer value : valueThree) {
+            valuesThree += value;
+        }
+        for(Integer value : valueFour) {
+            valuesFour += value;
+        }
+        for(Integer value : valueFive) {
+            valuesFive += value;
+        }
+        for(Integer value : valueSix) {
+            valuesSix += value;
+        }
     }
 
     private void setupChart() {
@@ -54,10 +167,7 @@ public class MonthFragment extends Fragment {
         XAxis label = chart.getXAxis();
         label.setPosition(XAxis.XAxisPosition.BOTTOM);
 
-        Legend legend = chart.getLegend();
-        legend.setEnabled(false);
-
-        BarDataSet dataSet = new BarDataSet(getDataSet(), "Test");
+        BarDataSet dataSet = new BarDataSet(getDataSet(), "Gelopen afstand in kilometers per maand");
 
         dataSet.setColors(new int[]{Color.parseColor("#27b764"), Color.parseColor("#a8e2c1")});
         dataSet.setDrawValues(false);
@@ -70,12 +180,12 @@ public class MonthFragment extends Fragment {
     private ArrayList<BarEntry> getDataSet() {
         entries = new ArrayList<>();
 
-        entries.add(new BarEntry(4f, 0));
-        entries.add(new BarEntry(8f, 1));
-        entries.add(new BarEntry(6f, 2));
-        entries.add(new BarEntry(14f, 3));
-        entries.add(new BarEntry(10f, 4));
-        entries.add(new BarEntry(18f, 5));
+        entries.add(new BarEntry(valuesSix, 0));
+        entries.add(new BarEntry(valuesFive, 1));
+        entries.add(new BarEntry(valuesFour, 2));
+        entries.add(new BarEntry(valuesThree, 3));
+        entries.add(new BarEntry(valuesTwo, 4));
+        entries.add(new BarEntry(valuesOne, 5));
 
         return entries;
     }
--- app/src/main/java/net/sightwalk/Controllers/Dashboard/WeekFragment.java
+++ app/src/main/java/net/sightwalk/Controllers/Dashboard/WeekFragment.java
@@ -3,31 +3,52 @@ package net.sightwalk.Controllers.Dashboard;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
 import net.danlew.android.joda.JodaTimeAndroid;
+import net.sightwalk.Models.Activities;
 import net.sightwalk.R;
+import net.sightwalk.Stores.ActivitiesStore;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class WeekFragment extends Fragment {
 
     private BarChart chart;
+
+    private ActivitiesStore store;
+
+    private int valuesOne;
+    private int valuesTwo;
+    private int valuesThree;
+    private int valuesFour;
+
     private ArrayList<BarEntry> entries;
     private ArrayList<String> labels;
 
-    View view;
+    private ArrayList<Integer> valueOne = new ArrayList<>();
+    private ArrayList<Integer> valueTwo = new ArrayList<>();
+    private ArrayList<Integer> valueThree = new ArrayList<>();
+    private ArrayList<Integer> valueFour = new ArrayList<>();
+
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,11 +56,85 @@ public class WeekFragment extends Fragment {
 
         JodaTimeAndroid.init(getActivity());
 
+        store = new ActivitiesStore(getContext());
+
+        for(Activities activities : store.getAll()) {
+            checkDate(activities);
+        }
+
+        setValues();
+
         chart = (BarChart) view.findViewById(R.id.weekChart);
 
         setupChart();
 
         return view;
+    }
+
+    private void checkDate(Activities activities) {
+        DateTimeFormatter format = DateTimeFormat.forPattern("dd/MM/yyyy");
+        LocalDate date = format.parseLocalDate(activities.getActivityDate());
+        int week = date.getWeekOfWeekyear();
+
+        int dateOne = DateTime.now().getWeekOfWeekyear();
+        int dateTwo = DateTime.now().minusMonths(1).getWeekOfWeekyear();
+        int dateThree = DateTime.now().minusMonths(2).getWeekOfWeekyear();
+        int dateFour = DateTime.now().minusMonths(3).getWeekOfWeekyear();
+
+        if(week == dateOne) {
+            valueOne.add(getDistance(activities.getJson()));
+        }
+        else if(week == dateTwo) {
+            valueTwo.add(getDistance(activities.getJson()));
+        }
+        else if(week == dateThree) {
+            valueThree.add(getDistance(activities.getJson()));
+        }
+        else if(week == dateFour) {
+            valueFour.add(getDistance(activities.getJson()));
+        }
+    }
+
+    private int getDistance(String json) {
+        int routedis = 0;
+
+        try {
+            JSONObject directionsObject = new JSONObject(json);
+
+            JSONArray routesObject = directionsObject.getJSONArray("routes");
+
+            for (int i = 0; i < routesObject.length(); i++) {
+                JSONObject overview = routesObject.getJSONObject(i);
+
+                JSONArray legsObject = overview.getJSONArray("legs");
+
+                for (int l = 0; l < legsObject.length(); l++) {
+                    JSONObject stepsobject = legsObject.getJSONObject(l);
+
+                    JSONObject routeDistance = stepsobject.getJSONObject("distance");
+
+                    routedis += routeDistance.getInt("value");
+                }
+            }
+        } catch (JSONException ex) {
+            Log.e("ERROR_", ex.getLocalizedMessage());
+        }
+        return routedis/1000;
+    }
+
+    private void setValues() {
+        for(Integer value : valueOne) {
+            valuesOne += value;
+        }
+        for(Integer value : valueTwo) {
+            valuesTwo += value;
+        }
+        for(Integer value : valueThree) {
+            valuesThree += value;
+        }
+        for(Integer value : valueFour) {
+            valuesFour += value;
+        }
     }
 
     private void setupChart() {
@@ -54,10 +149,7 @@ public class WeekFragment extends Fragment {
         XAxis label = chart.getXAxis();
         label.setPosition(XAxis.XAxisPosition.BOTTOM);
 
-        Legend legend = chart.getLegend();
-        legend.setEnabled(false);
-
-        BarDataSet dataSet = new BarDataSet(getDataSet(), "Test");
+        BarDataSet dataSet = new BarDataSet(getDataSet(), "Gelopen afstand in kilometers per week");
 
         dataSet.setColors(new int[]{Color.parseColor("#27b764"), Color.parseColor("#a8e2c1")});
         dataSet.setDrawValues(false);
@@ -70,10 +162,10 @@ public class WeekFragment extends Fragment {
     private ArrayList<BarEntry> getDataSet() {
         entries = new ArrayList<>();
 
-        entries.add(new BarEntry(4f, 0));
-        entries.add(new BarEntry(8f, 1));
-        entries.add(new BarEntry(6f, 2));
-        entries.add(new BarEntry(14f, 3));
+        entries.add(new BarEntry(valuesFour, 0));
+        entries.add(new BarEntry(valuesThree, 1));
+        entries.add(new BarEntry(valuesTwo, 2));
+        entries.add(new BarEntry(valuesOne, 3));
 
         return entries;
     }
